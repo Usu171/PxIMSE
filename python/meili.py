d35891de776a397df55dcaf701531a198641ad10
Usu171 <usu171@foxmail.com>
2024-12-14 01:39:15 +0800

    fix

--- python/meili.py
+++ python/meili.py
@@ -6,7 +6,7 @@ class Meili():
     
     def __init__(self, config):
         self.index = config['meilisearch-index']
-        self.client = Client(f'http://{config['meilisearch-host']}:{config['meilisearch-port']}', config['meilisearch-master-key'])
+        self.client = Client(f'http://{config["meilisearch-host"]}:{config["meilisearch-port"]}', config['meilisearch-master-key'])
     
     def update_documents(self, documents, primary_key):
         return self.client.index(self.index).update_documents(documents, primary_key)
